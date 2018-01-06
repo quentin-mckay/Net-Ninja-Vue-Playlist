a6dba21f468e3574ae7317d8cfe5c9450b55c559
Quentin McKay <quentinmckay1@gmail.com>
2018-01-07 09:45:32 +1100

    #19 Nested Components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// example: globally register component for use everywhere
+// import Ninjas from './Ninjas.vue'
+// Vue.component('ninjas', Ninjas)
+
 new Vue({
   el: '#app',
   render: h => h(App)
