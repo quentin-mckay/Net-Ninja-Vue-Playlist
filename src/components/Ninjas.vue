08a9c06e2999f70eaac1ae18296c3c7a895e910c
Quentin McKay <quentinmckay1@gmail.com>
2018-01-07 12:57:54 +1100

    #21 Nesting Components Examples

---
+++ src/components/Ninjas.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" v-on:click="ninja.show = !ninja.show">
+        <h2>{{ ninja.name }}</h2>
+        <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#ninjas {
+  background-color: #eee;
+  /* width: 100%; */
+  max-width: 1200px;
+  margin: 40px auto;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+
+  border: 1px solid #222;
+  padding: 30px;
+  margin: 10px;
+}
+</style>
