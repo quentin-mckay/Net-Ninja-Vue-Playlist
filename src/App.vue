5c74b0f6edd36cb5ec7410093d55dcc0b500a0ce
Quentin McKay <quentinmckay1@gmail.com>
2018-01-09 10:14:03 +1100

    #27 Slots

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,35 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
+    <form-helper>
+      <div slot="f-header">
+        <h3>This is the title of the form</h3>
+        <p>Info about the form</p>
+      </div>
+      <div slot="f-fields">
+        <input type="text" placeholder="Name" required>
+        <input type="password" placeholder="Password" required>
+      </div>
+      <div slot="f-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+import formHelper from './components/formHelper'
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'form-helper': formHelper
   },
   data () {
     return {
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ],
-      title: 'View Ninjas'
+      title: 'I am a dynamic slot title'
     }
   },
   methods: {
-    updateTitle: function (updatedTitle) {
-      this.title = updatedTitle
-    }
+
   }
 }
 </script>
---
+++ src/components/formHelper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Please fill out our form</h1>
+    <form>
+      <div id="form-header">
+        <slot name="f-header"></slot>
+      </div>
+      <div id="form-fields">
+        <slot name="f-fields"></slot>
+      </div>
+      <div id="form-controls">
+        <slot name="f-controls"></slot>
+      </div>
+      <div id="useful-links">
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      title: 'I am a dynamic slot title'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+
+</style>
