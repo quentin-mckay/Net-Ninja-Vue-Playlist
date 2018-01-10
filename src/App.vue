70b5640309a42fff04047192503ca7a8a3daaeb2
Quentin McKay <quentinmckay1@gmail.com>
2018-01-10 13:38:50 +1100

    added ShowPosts +GET requests + styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <new-post></new-post>
+    <!-- <new-post></new-post> -->
+    <show-posts></show-posts>
   </div>
 
-  
+
 </template>
 
 <script>
 import NewPost from './components/NewPost'
+import ShowPosts from './components/ShowPosts'
 
 export default {
   components: {
-    'new-post': NewPost
-
+    'new-post': NewPost,
+    'show-posts': ShowPosts
   },
   data () {
     return {
---
+++ src/components/ShowPosts.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="show-posts">
+    <h1>All Blog Posts</h1>
+
+    <div v-for="post in posts" class="single-post">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(responseData => {
+      this.posts = responseData.body.slice(0, 10)
+    })
+  }
+}
+</script>
+
+<style>
+* {
+  /* box-sizing: border-box; */
+}
+
+#show-posts {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-post {
+  background-color: #eee;
+  padding:20px;
+  margin: 20px 0;
+  /* box-sizing: border-box; */
+}
+</style>
